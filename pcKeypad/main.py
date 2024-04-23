c5e55c7fe045c9ca4842868db7fd2625a1a03bd1
[ansquf0715] <ansquf0715@naver.com>
2024-04-24 02:08:02 +0900

    빌드 수정

--- pcKeypad/main.py
+++ pcKeypad/main.py
@@ -2,8 +2,9 @@ import errno
 import socket
 import threading
 import sys
-import serial #pySerial 라이브러리 추가
 import random
+import subprocess
+import platform
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QHBoxLayout
 from PyQt5.QtCore import pyqtSignal, QObject, Qt
 from PyQt5.QtGui import QFont, QFontDatabase
@@ -13,63 +14,47 @@ class Server(QObject):
 
     def __init__(self, host, port, main_widget):
         super().__init__()
-        # self.host = host
+        self.host = host
         self.port = port
         self.main_widget = main_widget
-        self.serial_port = serial.Serial(str(port), 9600) #시리얼 포트 설정
-        # self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.client_sockets = [] #연결된 클라이언트 소켓을 추적하기 위한 리스트
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_sockets = [] #연결된 클라이언트 소켓을 추적하기 위한 리스트
         self.stop_flag = False
         self.lock = threading.Lock()
 
     def start_server(self):
-        # 시리얼 포트가 열렸는지 확인
-        if not self.serial_port.is_open:
-            print("Serial port is not open.")
-            return
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
+
+        # print("서버가 시작되었습니다.")
+        # print(f"서버의 IP 주소: {self.host}")
+        # print(f"서버의 포트 번호: {self.port}")
+
+        self.main_widget.show_server_info(self.host, self.port)
 
         while not self.stop_flag:
+            # print('stop flag', self.stop_flag)
             try:
-                data = self.serial_port.readline().decode().strip()  # 데이터 수신
-                if data:
-                    print("Received:", data)
-                    self.message_received.emit(data)  # 수신된 데이터를 PyQt 애플리케이션에 전달
-            except Exception as e:
-                print("Error while receiving data:", e)
+                conn, addr = self.server_socket.accept()
+                print('Connected by', addr)
+                self.client_sockets.append(conn) #연결된 클라이언트 소켓을 리스트에 추가
+                threading.Thread(target=self.handle_client, args=(conn,)).start()
+            except OSError as e:
+                if e.errno == errno.EINVAL:
+                    #소켓이 이미 닫혔는데도 accept() 호출을 시도한 경우
+                    break
+                else:
+                    raise
 
-    # def start_server(self):
-    #     self.server_socket.bind((self.host, self.port))
-    #     self.server_socket.listen()
-    #
-    #     # print("서버가 시작되었습니다.")
-    #     # print(f"서버의 IP 주소: {self.host}")
-    #     # print(f"서버의 포트 번호: {self.port}")
-    #
-    #     self.main_widget.show_server_info(self.host, self.port)
-    #
-    #     while not self.stop_flag:
-    #         # print('stop flag', self.stop_flag)
-    #         try:
-    #             conn, addr = self.server_socket.accept()
-    #             print('Connected by', addr)
-    #             self.client_sockets.append(conn) #연결된 클라이언트 소켓을 리스트에 추가
-    #             threading.Thread(target=self.handle_client, args=(conn,)).start()
-    #         except OSError as e:
-    #             if e.errno == errno.EINVAL:
-    #                 #소켓이 이미 닫혔는데도 accept() 호출을 시도한 경우
-    #                 break
-    #             else:
-    #                 raise
-
-    # def handle_client(self, conn):
-    #     with conn:
-    #         while True:
-    #             data = conn.recv(1024)
-    #             if not data:
-    #                 break
-    #             message = data.decode()
-    #             # print('수신된 데이터:', message)
-    #             self.message_received.emit(message)
+    def handle_client(self, conn):
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                message = data.decode()
+                # print('수신된 데이터:', message)
+                self.message_received.emit(message)
 
     # def send_message_to_client(self, message):
     #     if self.client_sockets:
@@ -80,49 +65,42 @@ class Server(QObject):
     #         except Exception as e:
     #             print("Error while sending message to tablet:", e)
 
-    # def send_message_to_client(self, message):
-    #     with self.lock:
-    #         for client_socket in self.client_sockets:
-    #             try:
-    #                 client_socket.sendall(message.encode())
-    #             except Exception as e:
-    #                 print("Error while sending message to client:", e)
-
     def send_message_to_client(self, message):
-        # 데이터를 시리얼 포트를 통해 클라이언트에 전송
-        try:
-            self.serial_port.write(message.encode())
-        except Exception as e:
-            print("Error while sending message to client:", e)
-
-    # def stop_server(self):
-    #     self.stop_flag=True
-    #     # print('stop flag', self.stop_flag)
-    #     # print('client sockets', self.client_sockets)
-    #
-    #     # #서버 소켓을 닫기 전에 모든 클라이언트와의 연결을 먼저 종료
-    #     # for client_socket in self.client_sockets:
-    #     #     client_socket.close()
-    #     # self.client_sockets = [] #모든 클라이언트 소켓 비우기
-    #
-    #     with self.lock:
-    #         # 서버 소켓을 닫기 전에 모든 클라이언트와의 연결을 먼저 종료
-    #         for client_socket in self.client_sockets:
-    #             client_socket.close()
-    #         self.client_sockets = []  # 모든 클라이언트 소켓 비우기
-    #
-    #     self.server_socket.close()
+        with self.lock:
+            for client_socket in self.client_sockets:
+                try:
+                    client_socket.sendall(message.encode())
+                except Exception as e:
+                    print("Error while sending message to client:", e)
 
     def stop_server(self):
-        self.stop_flag = True
-        if self.serial_port.is_open:
-            self.serial_port.close()
+        self.stop_flag=True
+        # print('stop flag', self.stop_flag)
+        # print('client sockets', self.client_sockets)
+
+        # #서버 소켓을 닫기 전에 모든 클라이언트와의 연결을 먼저 종료
+        # for client_socket in self.client_sockets:
+        #     client_socket.close()
+        # self.client_sockets = [] #모든 클라이언트 소켓 비우기
+
+        #클라이언트에게 Quit 신호 보내기
+        quit_message = "Quit"
+        self.send_message_to_client(quit_message)
+
+        with self.lock:
+            # 서버 소켓을 닫기 전에 모든 클라이언트와의 연결을 먼저 종료
+            for client_socket in self.client_sockets:
+                client_socket.close()
+            self.client_sockets = []  # 모든 클라이언트 소켓 비우기
+
+        self.server_socket.close()
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
         self.server = None
+        self.add_firewall_rule()
 
     def initUI(self):
         self.setWindowTitle('PC KEYPAD')
@@ -192,6 +170,16 @@ class MyApp(QWidget):
         self.btn_receive.clicked.connect(self.sendSignalToClients)
         self.btn_copy.clicked.connect(self.btnClicked)
 
+    def add_firewall_rule(self):
+        #방화벽 규칙 추가
+        port_to_allow = 65432
+        if platform.system() == "Windows":
+            command = f'New-NetFirewallRule -DisplayName "Allow TCP Port {port_to_allow}" -Direction Inbound -Protocol TCP -LocalPort {port_to_allow} -Action Allow'
+            subprocess.run(["powershell", "-Command", command], shell=True)
+            print(f"포트 {port_to_allow}를 방화벽에서 허용했습니다.")
+        else:
+            print("Windows 운영체제가 아닙니다. 방화벽 규칙을 추가할 수 없습니다.")
+
     def show_server_info(self, host, port):
         self.ip_label.setText(f"IP 주소: {host}")
         self.port_label.setText(f"포트 번호: {port}")
@@ -211,11 +199,12 @@ class MyApp(QWidget):
         #클립보드에 저장할 텍스트
         original_text = self.received_message_label.text()
         #하이픈 제거
-        modified_text = original_text[:3]+original_text[4:8]+original_text[9:]
+        # modified_text = original_text[:3]+original_text[4:8]+original_text[9:]
 
         #클립보드에 수신된 데이터 저장
         clipboard = QApplication.clipboard()
-        clipboard.setText(modified_text.replace('\n', '\r\n'))
+        # clipboard.setText(modified_text.replace('\n', '\r\n'))
+        clipboard.setText(original_text.replace('\n', '\r\n'))
 
     def sendSignalToClients(self):
         # print("send signal to Clients")
