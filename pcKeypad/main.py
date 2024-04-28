001ea4f79e152dc5f89bbd55af2d2fe6a08b6b8d
[ansquf0715] <ansquf0715@naver.com>
2024-04-29 00:08:48 +0900

    pc용 서버 수정

--- pcKeypad/main.py
+++ pcKeypad/main.py
@@ -9,101 +9,20 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushBut
 from PyQt5.QtCore import pyqtSignal, QObject, Qt
 from PyQt5.QtGui import QFont, QFontDatabase
 
-class Server(QObject):
-    message_received = pyqtSignal(str)
+from flask import Flask, request
 
-    def __init__(self, host, port, main_widget):
-        super().__init__()
-        self.host = host
-        self.port = port
-        self.main_widget = main_widget
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_sockets = [] #연결된 클라이언트 소켓을 추적하기 위한 리스트
-        self.stop_flag = False
-        self.lock = threading.Lock()
+app_flask = Flask(__name__)
 
-    def start_server(self):
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen()
+@app_flask.route('/')
+def index():
+    return 'Hello, Flask!'
 
-        # print("서버가 시작되었습니다.")
-        # print(f"서버의 IP 주소: {self.host}")
-        # print(f"서버의 포트 번호: {self.port}")
-
-        self.main_widget.show_server_info(self.host, self.port)
-
-        while not self.stop_flag:
-            # print('stop flag', self.stop_flag)
-            try:
-                conn, addr = self.server_socket.accept()
-                print('Connected by', addr)
-                self.client_sockets.append(conn) #연결된 클라이언트 소켓을 리스트에 추가
-                threading.Thread(target=self.handle_client, args=(conn,)).start()
-            except OSError as e:
-                if e.errno == errno.EINVAL:
-                    #소켓이 이미 닫혔는데도 accept() 호출을 시도한 경우
-                    break
-                else:
-                    raise
-
-    def handle_client(self, conn):
-        with conn:
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                message = data.decode()
-                # print('수신된 데이터:', message)
-                self.message_received.emit(message)
-
-    # def send_message_to_client(self, message):
-    #     if self.client_sockets:
-    #         tablet_client = self.client_sockets[0]
-    #         try:
-    #             tablet_client.sendall(message.encode())
-    #             # print("Message sent to tablet:", message)
-    #         except Exception as e:
-    #             print("Error while sending message to tablet:", e)
-
-    def send_message_to_client(self, message):
-        with self.lock:
-            for client_socket in self.client_sockets:
-                try:
-                    client_socket.sendall(message.encode())
-                except Exception as e:
-                    print("Error while sending message to client:", e)
-
-    def remove_firewall_rule(self, port):
-        if platform.system() == "Windows":
-            command = f'Remove-NetFirewallRule -DisplayName "Allow TCP Port {port}"'
-            subprocess.Popen(["powershell", "-Command", command], shell=False, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-            print(f"포트 {port}에 대한 방화벽 규칙을 삭제했습니다.")
-        else:
-            print("Windows 운영체제가 아닙니다. 방화벽 규칙을 삭제할 수 없습니다.")
-
-    def stop_server(self):
-        self.stop_flag=True
-        # print('stop flag', self.stop_flag)
-        # print('client sockets', self.client_sockets)
-
-        # #서버 소켓을 닫기 전에 모든 클라이언트와의 연결을 먼저 종료
-        # for client_socket in self.client_sockets:
-        #     client_socket.close()
-        # self.client_sockets = [] #모든 클라이언트 소켓 비우기
-
-        #클라이언트에게 Quit 신호 보내기
-        quit_message = "Quit"
-        self.send_message_to_client(quit_message)
-
-        with self.lock:
-            # 서버 소켓을 닫기 전에 모든 클라이언트와의 연결을 먼저 종료
-            for client_socket in self.client_sockets:
-                client_socket.close()
-            self.client_sockets = []  # 모든 클라이언트 소켓 비우기
-
-        self.server_socket.close()
-        self.remove_firewall_rule(self.port)
+@app_flask.route('/message', methods=['POST'])
+def receive_message():
+    message = request.json['message']
+    print("Received message from client:", message)
+    #여기에 pyqt 앱으로 메시지를 전달하는 코드 추가 가능
+    return 'Message received'
 
 class MyApp(QWidget):
     def __init__(self):
@@ -180,17 +99,6 @@ class MyApp(QWidget):
         self.btn_receive.clicked.connect(self.sendSignalToClients)
         self.btn_copy.clicked.connect(self.btnClicked)
 
-    def add_firewall_rule(self):
-        # 방화벽 규칙 추가
-        port_to_allow = 65432
-        if platform.system() == "Windows":
-            command = f'New-NetFirewallRule -DisplayName "Allow TCP Port {port_to_allow}" -Direction Inbound -Protocol TCP -LocalPort {port_to_allow} -Action Allow'
-            subprocess.Popen(["powershell", "-Command", command], shell=False, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-            print(f"포트 {port_to_allow}를 방화벽에서 허용했습니다.")
-        else:
-            print("Windows 운영체제가 아닙니다. 방화벽 규칙을 추가할 수 없습니다.")
-
     def show_server_info(self, host, port):
         self.ip_label.setText(f"IP 주소: {host}")
         self.port_label.setText(f"포트 번호: {port}")
@@ -229,18 +137,10 @@ class MyApp(QWidget):
         event.accept()
 
 if __name__ == '__main__':
+    app_flask_thread = threading.Thread(target=app_flask.run, kwargs={'host': '0.0.0.0', 'port': 5000})
+    app_flask_thread.start()
+
     app = QApplication(sys.argv)
     ex = MyApp()
     ex.show()
-
-    #통상적으로 사용되는 포트 번호 범위: 1024-49151
-    # random_port = random.randint(1024, 49151)
-    random_port = 65432
-
-    server = Server(socket.gethostbyname(socket.gethostname()), random_port, ex)
-    ex.server = server
-    server.message_received.connect(ex.show_received_message)
-    server_thread = threading.Thread(target=server.start_server)
-    server_thread.start()
-
     sys.exit(app.exec_())
